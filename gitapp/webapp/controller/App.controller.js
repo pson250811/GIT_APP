95fed3e01628694433122be30d3da18aae66401f
pson@deloitte.com <pson@deloitte.com>
2025-10-06 12:14:07 +0000

    /home/user/projects/gitapp

---
+++ gitapp/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("sap.btp.gitapp.controller.App", {
+      onInit() {
+      }
+  });
+});
